6eb9bc5f338a857281270f7da059a9d7dfe4460d
Temesgen dora <tdemeke36@gmail.com>
2024-05-30 05:01:15 -0700

    done: task 3

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,30 @@
+export default class Currency {
+  constructor(code, name) {
+    if (typeof code !== 'string') throw new TypeError('Code must be string');
+    else if (typeof name !== 'string') throw new TypeError('Name must be string');
+    this._code = code;
+    this._name = name;
+  }
+
+  getCode() {
+    return this._code;
+  }
+
+  getName() {
+    return this._name;
+  }
+
+  setCode(code) {
+    if (typeof code !== 'string') throw new TypeError('Code must be string');
+    this._code = code;
+  }
+
+  setName(name) {
+    if (typeof name !== 'string') throw new TypeError('Name must be string');
+    this._name = name;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
