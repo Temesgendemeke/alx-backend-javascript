073aa6fbafd4b1ba1c7b6d00e55a42cf3ab5cd6f
Temesgen dora <tdemeke36@gmail.com>
2024-05-30 04:47:43 -0700

    done: task 2

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,53 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw new TypeError('name must be a string');
+    }
+    if (typeof length !== 'number') {
+      throw new TypeError('length must be a number');
+    }
+    if (!Array.isArray(students)) {
+      throw new TypeError('students must be an array');
+    }
+
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  // Getter methods
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    // Return a copy of the students array to prevent external modification
+    return [...this._students];
+  }
+
+  // Setter methods
+  set name(newName) {
+    if (typeof newName !== 'string') {
+      throw new TypeError('name must be a string');
+    }
+    this._name = newName;
+  }
+
+  set length(newLength) {
+    if (typeof newLength !== 'number') {
+      throw new TypeError('length must be a number');
+    }
+    this._length = newLength;
+  }
+
+  set students(newStudents) {
+    if (!Array.isArray(newStudents)) {
+      throw new TypeError('students must be an array');
+    }
+    this._students = newStudents;
+  }
+}
