ab4439f0074822866cd960b7d45241e358c50447
Temesgen dora <tdemeke36@gmail.com>
2024-05-30 05:42:25 -0700

    done: task 4

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,34 @@
+class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  setAmount() {
+    return this._amount;
+  }
+
+  setCurrency() {
+    return this._currency;
+  }
+
+  getAmount(amount) {
+    this._amount = amount;
+  }
+
+  getCurrency(currency) {
+    this._currency = currency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.getName()} (${this._currency.getCode()})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number') throw new TypeError('amount must be a number');
+    else if (typeof conversionRate !== 'number') throw new TypeError('conversionRate must be number');
+    return amount * conversionRate;
+  }
+}
+
+export default Pricing;
