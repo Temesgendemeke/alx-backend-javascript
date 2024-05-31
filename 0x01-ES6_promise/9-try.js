463b82eb9503b323d9b7308b1952f3cabfc9a5df
Temesgen dora <tdemeke36@gmail.com>
2024-05-31 14:47:10 -0700

    done: task 9

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -3,8 +3,10 @@ export default function guardrail(mathFunction) {
   try {
     queue.push(mathFunction());
   } catch (error) {
-    queue.push(error);
+    queue.push(String(error));
+  } finally {
+    queue.push('Guardrail was processed');
   }
-  queue.push('Guardrail was processed');
+
   return queue;
 }
