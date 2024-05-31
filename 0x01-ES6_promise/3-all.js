11ae5fe893480eea837fd5c4ffe95e5dc9856a33
Temesgen dora <tdemeke36@gmail.com>
2024-05-31 13:38:12 -0700

    done: task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,12 +1,8 @@
-import {uploadPhoto, createUser} from './utils'
+import { uploadPhoto, createUser } from './utils';
 
-export default async function handleProfileSignup(){
-    try{
-        const data = await uploadPhoto()
-        const user = await createUser()
-        console.log(data.body, user.firstName, user.lastName)
-    }
-    catch{
-        console.log('Signup system offline')
-    }
+export default function handleProfileSignup() {
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then((data) => console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`))
+    .catch('Signup system offline ');
 }
