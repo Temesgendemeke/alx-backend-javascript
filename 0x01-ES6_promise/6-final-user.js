fb9c8fc3c4923aeb9118697ba40ccc0cf2bd5c9e
Temesgen dora <tdemeke36@gmail.com>
2024-05-31 14:10:22 -0700

    fixed: task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,11 +3,10 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
-    .all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((data) => (
       data.map((d) => ({
         status: d.status,
         value: d.status === 'fulfilled' ? d.value : String(d.reason),
       }))))
-    .catch(() => new Error());
 }
