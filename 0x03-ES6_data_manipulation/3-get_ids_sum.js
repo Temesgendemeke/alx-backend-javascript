76132347035ac23a2addf076f19f279953d1b280
Temesgen dora <tdemeke36@gmail.com>
2024-06-03 09:47:05 -0700

    done: task 3

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,5 +1,3 @@
 export default function getStudentsByLocation(list) {
-    return list.reduce((accumlater, currentValue) => accumlater + currentValue)
-    
+  return list.reduce((accumlater, currentValue) => accumlater + currentValue.id, 0);
 }
-  
