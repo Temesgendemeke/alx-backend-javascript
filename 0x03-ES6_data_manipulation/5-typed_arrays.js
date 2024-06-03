edc16802a0754e835cb04e8a94ce22e7f4455fd9
Temesgen dora <tdemeke36@gmail.com>
2024-06-03 11:35:07 -0700

    fixed: task 5

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,8 +1,8 @@
 export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
-  const arr = new Uint8Array(buffer);
+  const int8Array = new Int8Array(buffer);
   try{
-    arr[position] = value;
+    int8Array[position] = value;
   }catch(err){
     throw new Error("Position outside range")
   }
