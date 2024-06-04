9d79361061a460fad1fe9a31892999b55c77abe2
Temesgen dora <tdemeke36@gmail.com>
2024-06-04 10:49:21 -0700

    fixed: task 8 #2

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,11 @@
 export default function cleanSet(set, startString) {
   const text = [];
+
+  if (!set || (typeof set !== 'object') || (typeof startString !== 'string') || startString === '') {
+    return '';
+  }
   set.forEach((value) => {
-    if (value.startsWith(startString) && startString !== '') {
+    if (value.startsWith(startString)) {
       text.push(value.substring(startString.length));
     }
   });
