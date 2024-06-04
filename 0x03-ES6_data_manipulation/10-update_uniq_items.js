ec540388fff8dd69efedb6a14059784e750e3d57
Temesgen dora <tdemeke36@gmail.com>
2024-06-04 11:19:12 -0700

    fixed: task 10

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,4 +1,5 @@
 export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) throw new Error('Cannot process');
   for (const [keys, values] of map) {
     if (values === 1) {
       map.set(keys, 100);
