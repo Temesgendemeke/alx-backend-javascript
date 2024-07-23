24367e04050f2b70b7426ea5bdf6ea50d4303daa
Temesgen dora <tdemeke36@gmail.com>
2024-07-23 07:44:44 -0700

    updated task 0

--- 0x05-Node_JS_basic/tests/0-main.js
+++ 0x05-Node_JS_basic/tests/0-main.js
@@ -1,3 +1,9 @@
-const displayMessage = require('../0-console');
+/**
+ * Logs a message to the console.
+ * @param {string} message - The message to log.
+ * @returns {void}
+ */
+// Arrow function
+const displayMessage = (message) => console.log(message);
 
-displayMessage("Hello NodeJS!");
+module.exports = displayMessage;
