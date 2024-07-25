d3ae3796bc31fc48f4a9148faddfac795028c0c4
Temesgen dora <tdemeke36@gmail.com>
2024-07-25 12:25:46 -0700

    done: task 2

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -3,32 +3,33 @@ const calculateNumber = require('./1-calcul');
 
 describe('calculateNumber', function() {
     it('should return the sum of rounded numbers', function() {
-        assert.strictEqual(calculateNumber(1.4, 4.5, 'SUM'), 6);
-        assert.strictEqual(calculateNumber(1.5, 3.7, 'SUM'), 6);
-        assert.strictEqual(calculateNumber(2.8, 2.3, 'SUM'), 5);
-        assert.strictEqual(calculateNumber(-1.2, 2.3, 'SUM'), 1);
-        assert.strictEqual(calculateNumber(-1.5, -2.5, 'SUM'), -3);
+        assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
+        assert.strictEqual(calculateNumber('SUM', 1.5, 3.7), 6);
+        assert.strictEqual(calculateNumber('SUM', 2.8, 2.3), 5);
+        assert.strictEqual(calculateNumber('SUM', -1.2, 2.3), 1);
+        assert.strictEqual(calculateNumber('SUM', -1.5, -2.5), -3);
     });
     
-    it('should return the difference of rounded numbers', function() {
-        assert.strictEqual(calculateNumber(1.4, 4.5, 'SUBTRACT'), -4);
-        assert.strictEqual(calculateNumber(1.5, 3.7, 'SUBTRACT'), -2);
-        assert.strictEqual(calculateNumber(2.8, 2.3, 'SUBTRACT'), 1);
-        assert.strictEqual(calculateNumber(-1.2, 2.3, 'SUBTRACT'), -3);
-        assert.strictEqual(calculateNumber(-1.5, -2.5, 'SUBTRACT'), 1);
+    it('should return the subtract of rounded numbers', function() {
+        assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+        assert.strictEqual(calculateNumber('SUBTRACT', 1.5, 3.7), -2);
+        assert.strictEqual(calculateNumber('SUBTRACT', 2.8, 2.3), 1);
+        assert.strictEqual(calculateNumber('SUBTRACT', -1.2, 2.3), -3);
+        assert.strictEqual(calculateNumber('SUBTRACT', -1.5, -2.5), 1);
     });
     
-    it('should return the quotient of rounded numbers', function() {
-        assert.strictEqual(calculateNumber(1.4, 4.5, 'DIVIDE'), 0.2);
-        assert.strictEqual(calculateNumber(1.5, 3.7, 'DIVIDE'), 0.5);
-        assert.strictEqual(calculateNumber(2.8, 2.3, 'DIVIDE'), 1.5);
-        assert.strictEqual(calculateNumber(-1.2, 2.3, 'DIVIDE'), -0.5);
-        assert.strictEqual(calculateNumber(-1.5, -2.5, 'DIVIDE'), 0.5);
+    it('should return the divide of rounded numbers', function() {
+        assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+        assert.strictEqual(calculateNumber('DIVIDE', 1.5, 3.7), 0.5);
+        assert.strictEqual(calculateNumber('DIVIDE', 2.8, 2.3), 1.5);
+        assert.strictEqual(calculateNumber('DIVIDE', -1.2, 2.3), -0.5);
+        assert.strictEqual(calculateNumber('DIVIDE', -1.5, -2.5), 0.5);
     });
     
     it('should handle edge cases', function() {
-        assert.strictEqual(calculateNumber(0, 0, 'DIVIDE'), 'Error');
-        assert.strictEqual(calculateNumber(1.4999999, 1.4999999, 'DIVIDE'), 1);
-        assert.strictEqual(calculateNumber(-1.4999999, -1.4999999, 'DIVIDE'), 1);
-        assert.strictEqual(calculateNumber(0.1, 0.1, 'DIVIDE'), 'Error')});
+        assert.strictEqual(calculateNumber('SUM', 0, 0), 0);
+        assert.strictEqual(calculateNumber('SUM', 1.4999999, 1.4999999), 2);
+        assert.strictEqual(calculateNumber('SUM', -1.4999999, -1.4999999), -2);
+        assert.strictEqual(calculateNumber('SUM', 0.1, 0.1), 0)
+    });
 })
