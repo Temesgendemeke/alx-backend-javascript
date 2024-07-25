03d39d71326acb44d574598bdc2bd8c4844a2edb
Temesgen dora <tdemeke36@gmail.com>
2024-07-25 14:32:35 -0700

    done: task 5

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,30 @@
+const sinon = require("sinon");
+const { expect } = require("chai");
+const Utils = require("./utils");
+const sendPaymentRequestToApi = require("./5-payment");
+
+describe("sendPaymentRequestToApi", function () {
+	let consoleSpy;
+
+	beforeEach(function () {
+		consoleSpy = sinon.spy(console, "log");
+	});
+
+	afterEach(function () {
+		consoleSpy.restore();
+	});
+
+	it("should call sendPaymentRequestToApi with 100 and 20 and log the correct message", function () {
+		sendPaymentRequestToApi(100, 20);
+
+		expect(consoleSpy.calledOnceWithExactly("The total is: 120")).to.be.true;
+		expect(consoleSpy.calledOnce).to.be.true;
+	});
+
+	it("should call sendPaymentRequestToApi with 10 and 10 and log the correct message", function () {
+		sendPaymentRequestToApi(10, 10);
+
+		expect(consoleSpy.calledOnceWithExactly("The total is: 20")).to.be.true;
+		expect(consoleSpy.calledOnce).to.be.true;
+	});
+});
